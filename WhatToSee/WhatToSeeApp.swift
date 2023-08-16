625d9a83b7eacde7dc1e04a68a2c6585cb339402
AndresAleu <andresaleunz@gmail.com>
2023-08-16 13:06:47 +0200

    Initial Commit

---
+++ WhatToSee/WhatToSeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToSeeApp.swift
+//  WhatToSee
+//
+//  Created by Andres Aleu on 16/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatToSeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
