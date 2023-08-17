5d445cd8c0354fdec8083ebc864c4f2c30104e43
AndresAleu <andresaleunz@gmail.com>
2023-08-17 12:38:40 +0200

    Add view first Screen

---
+++ WhatToSee/UsesCases/LoginAndCreateAccount/Views/MainView.swift
@@ -0,0 +1,47 @@
+//
+//  MainView.swift
+//  WhatToSee
+//
+//  Created by Andres Aleu on 16/8/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    //Detecta configuracion de modo oscuro o claro del iPhone
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        VStack {
+            Button {
+                //TODO
+            } label: {
+                Text("Crear Cuenta".uppercased())
+                    .frame(width: 370, height: 50)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(10)
+            }
+            Button {
+                //TODO
+            } label: {
+                Text("Iniciar Sesion".uppercased())
+                    .frame(width: 370, height: 50)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(.green)
+                    .cornerRadius(10)
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+        MainView()
+            .previewDevice("iPhone 14")
+            
+    }
+}
