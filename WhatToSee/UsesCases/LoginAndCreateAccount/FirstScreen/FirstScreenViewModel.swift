0c21af3175efb0d95fe55be79fc445ed3eec4c16
AndresAleu <andresaleunz@gmail.com>
2023-08-17 13:59:53 +0200

    Add Views and viewModel

---
+++ WhatToSee/UsesCases/LoginAndCreateAccount/FirstScreen/FirstScreenViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  FirstScreenViewModel.swift
+//  WhatToSee
+//
+//  Created by Andres Aleu on 17/8/23.
+//
+
+import Foundation
+
+final class FirstScreenViewModel: ObservableObject {
+    
+//    func loginView() -> LoginView {
+//        return FirstScreenRouter.loginView()
+//        
+//    }
+}
---
+++ WhatToSee/UsesCases/LoginAndCreateAccount/Login/LoginViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewModel.swift
+//  WhatToSee
+//
+//  Created by Andres Aleu on 17/8/23.
+//
+
+import Foundation
+
+final class LoginViewModel: ObservableObject {
+    
+    private let router: FirstScreenRouter
+    
+    init(router: FirstScreenRouter) {
+        self.router = router
+    }
+    
+    
+}
