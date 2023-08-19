96679c46c00946c2a3227847cbf9ed72479b2797
AndresAleu <andresaleunz@gmail.com>
2023-08-19 13:05:10 +0200

    Add components button and textfields

---
+++ WhatToSee/UsesCases/Components/SecureFieldView.swift
@@ -0,0 +1,20 @@
+//
+//  SecureFieldView.swift
+//  WhatToSee
+//
+//  Created by Andres Aleu on 19/8/23.
+//
+
+import SwiftUI
+
+struct SecureFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecureFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecureFieldView()
+    }
+}
---
+++ WhatToSee/UsesCases/Components/TextFieldView.swift
@@ -0,0 +1,20 @@
+//
+//  TextFieldView.swift
+//  WhatToSee
+//
+//  Created by Andres Aleu on 19/8/23.
+//
+
+import SwiftUI
+
+struct TextFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldView()
+    }
+}
