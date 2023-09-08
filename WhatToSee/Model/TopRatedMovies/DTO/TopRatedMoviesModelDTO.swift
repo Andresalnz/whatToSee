15fc6a2fd17a7dc65d6d4c2b21184fafd1fd74c3
AndresAleu <andresaleunz@gmail.com>
2023-09-08 12:02:03 +0200

    Add model TopRated

--- WhatToSee/Model/TopRatedMovies/DTO/TopRatedMoviesModelDTO.swift
+++ WhatToSee/Model/TopRatedMovies/DTO/TopRatedMoviesModelDTO.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+struct TopRatedMoviesDTO: Codable {
+    let page: Int?
+    let results: [ResultsTopRatedMoviesDTO]?
+    let totalPages: Int?
+    let totalResults: Int?
+    
+    enum CodingKeys:  String, CodingKey {
+        case page
+        case results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+
+struct ResultsTopRatedMoviesDTO: Codable {
+    let adult: Bool?
+    let backdropPath: String?
+    let genreIds: [Int]?
+    let id: Int?
+    let originalLanguaje: String?
+    let originalTitle: String?
+    let overview: String?
+    let popularity: Double?
+    let posterPath: String?
+    let releaseDate: String?
+    let title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Double?
+    
+    enum CodingKeys:  String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIds = "genre_ids"
+        case id
+        case originalLanguaje = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
--- WhatToSee/Model/TopRatedMovies/Mapper/TopRatedMoviesMapper.swift
+++ WhatToSee/Model/TopRatedMovies/Mapper/TopRatedMoviesMapper.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+extension ResultsTopRatedMoviesDTO {
+    func toBo() -> ResultsTopRatedMoviesBO {
+        return ResultsTopRatedMoviesBO(adult: adult,
+                                      backdropPath: URL(string: backdropPath ?? ""),
+                                      genreIds: genreIds,
+                                      id: id,
+                                      originalLanguaje: originalLanguaje,
+                                      originalTitle: originalTitle,
+                                      overview: overview,
+                                      popularity: popularity,
+                                      posterPath: URL(string: posterPath ?? ""),
+                                      releaseDate: releaseDate,
+                                      title: title,
+                                      video: video,
+                                      voteAverage: voteAverage,
+                                      voteCount: voteCount)
+    }
+}
