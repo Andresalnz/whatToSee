fdd14a930a5288cdc559f9350fd7b02938c0ce87
AndresAleu <andresaleunz@gmail.com>
2023-09-11 13:08:31 +0200

    Add Constants and rows Home

---
+++ WhatToSee/Util/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  WhatToSee
+//
+//  Created by Andres Aleu on 9/9/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+   static let buildImage: String = "https://image.tmdb.org/t/p/w500"
+}
