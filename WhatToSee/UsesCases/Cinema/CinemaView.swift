2949dc88cfc2f23369e4599d498679385fad8cf3
Andresalnz <andresaleunz@gmail.com>
2023-09-11 18:38:02 +0200

    Add view and viewModel

---
+++ WhatToSee/UsesCases/Cinema/CinemaView.swift
@@ -0,0 +1,20 @@
+//
+//  CinemaView.swift
+//  WhatToSee
+//
+//  Created by Andres Aleu on 11/9/23.
+//
+
+import SwiftUI
+
+struct CinemaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CinemaView_Previews: PreviewProvider {
+    static var previews: some View {
+        CinemaView()
+    }
+}
---
+++ WhatToSee/UsesCases/Cinema/CinemaViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CinemaViewModel.swift
+//  WhatToSee
+//
+//  Created by Andres Aleu on 11/9/23.
+//
+
+import Foundation
