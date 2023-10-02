80ae6bf1be54c4cede4a0b3c89f1676665a515fb
Andresalnz <andresaleunz@gmail.com>
2023-10-02 16:14:41 +0200

    Add view and viewmodel

--- WhatToSee/UsesCases/Cinema/CinemaViewModel.swift
+++ WhatToSee/UsesCases/Cinema/CinemaViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+final class CinemaViewModel: ObservableObject {
+    
+    @Published var upcomingCinema: [ResultsUpcomingBO] = []
+    
+    var repository: CinemaRepository = CinemaRepository()
+    
+    func loadUI() {
+        Task {
+            await loadData()
+        }
+    }
+    
+    
+    @MainActor
+    func loadData() async {
+        do {
+            upcomingCinema = try await repository.getUpcomingCinema()
+        } catch (let err) {
+            print(err)
+        }
+    }
+}
